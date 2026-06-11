f28c927a63038dcd994871dc7cddbeecc4171edf
isPointer <aantikmods@gmail.com>
2026-06-11 09:28:23 +0600

    🤔

--- antik/src/main/java/com/antik/utils/output.java
+++ antik/src/main/java/com/antik/utils/output.java
@@ -1,6 +1,10 @@
 package com.antik.utils;
 
+import com.reandroid.apk.ApkModule;
+import com.reandroid.archive.WriteProgress;
+
 import java.io.File;
+import java.io.IOException;
 
 public class output {
     public static String get_out(File i, String n) {
@@ -9,5 +13,12 @@ public class output {
             return n;
         }
         return new File(p, n).getPath();
+    }
+    public static void write(ApkModule module, File file) throws IOException {
+        module.writeApk(file, new WriteProgress() {
+            @Override
+            public void onCompressFile(String path, int method, long length) {
+            }
+        });
     }
 }
